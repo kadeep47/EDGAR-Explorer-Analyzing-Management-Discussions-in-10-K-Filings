2761b1fc28403306d4a584fe1e78e412b078fd63
kadeep47 <kadeep47@gmail.com>
2023-06-20 19:39:03 +0530

    future options // more indexes

---
+++ code/possible_index.r
@@ -0,0 +1,27 @@
+# S&P 500 Index: The S&P 500 is a widely followed index that measures the performance of 500 large-cap U.S. stocks.
+getSymbols("^GSPC" )  # Retrieves S&P 500 data
+
+
+# Dow Jones Industrial Average (DJIA): The DJIA is a price-weighted index that represents 30 large, publicly traded companies in the United States.
+getSymbols("^DJI" )  # Retrieves DJIA data
+
+
+# NASDAQ Composite Index: The NASDAQ Composite is an index that includes all stocks listed on the NASDAQ stock market, encompassing a wide range of industries.
+getSymbols("^IXIC" )  # Retrieves NASDAQ Composite data
+
+
+# Russell 2000 Index: The Russell 2000 is an index that measures the performance of 2,000 small-cap U.S. stocks.
+getSymbols("^RUT")  # Retrieves Russell 2000 data
+
+
+# FTSE 100 Index: The FTSE 100 is an index of the 100 largest companies listed on the London Stock Exchange by market capitalization.
+getSymbols("^FTSE" )  # Retrieves FTSE 100 data
+
+
+# Nikkei 225 Index: The Nikkei 225 is a stock market index that represents 225 large, publicly traded companies on the Tokyo Stock Exchange.
+getSymbols("^N225" )  # Retrieves Nikkei 225 data
+
+
+# Hang Seng Index: The Hang Seng is a benchmark stock market index that represents the 50 largest companies listed on the Hong Kong Stock Exchange.
+
+getSymbols("^HSI" )  # Retrieves Hang Seng data
